491c8e8fcabf24553f4cb03d3ce00f0026ee61a8
MrSteve89 <stefan.bairac@outlook.com>
2020-08-10 18:26:00 +0300

    adding files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {createLogger} from 'redux-logger';
+import thunkMidlleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
 import * as serviceWorker from './serviceWorker';
+import {searchRobots, requestRobots} from './reducers';
+
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots});
+const store = createStore(rootReducer, applyMiddleware(thunkMidlleware, logger));
 
 ReactDOM.render(
   <React.StrictMode>
-    <Fragment>
+    <Provider store={store}>
     	<App />
-    </Fragment>	
+    </Provider>	
   </React.StrictMode>,
   document.getElementById('root')
 );
